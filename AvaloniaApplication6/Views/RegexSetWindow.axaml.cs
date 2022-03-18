6719ff47d11df3fdd5e96c01903286eb047b6e38
Mikhail <basargin123@gmail.com>
2022-03-18 17:39:29 +0700

    Добавьте файлы проекта.

---
+++ AvaloniaApplication6/Views/RegexSetWindow.axaml.cs
@@ -0,0 +1,50 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using AvaloniaApplication6.ViewModels;
+using System;
+using System.Text.RegularExpressions;
+
+namespace AvaloniaApplication6.Views
+{
+    public partial class RegexSetWindow : Window
+    {
+        public RegexSetWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            this.FindControl<Button>("OK").Click += delegate
+            {
+                var context = this.Owner.DataContext as MainWindowViewModel;
+                var inputField = this.FindControl<TextBox>("RegexInputField");
+                try
+                {
+                    Regex rg = new Regex(inputField.Text);
+                    context.Regex = inputField.Text;
+                    this.CloseWindow();
+                }
+                catch (Exception ex)
+                {
+                    inputField.Text = "INVALID REGEX";
+                }
+
+
+            };
+            this.FindControl<Button>("Close").Click += delegate
+            {
+                CloseWindow();
+            };
+        }
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+        private void CloseWindow()
+        {
+            this.Close();
+        }
+
+    }
+}
